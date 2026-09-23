4617ad1e23e0056a25d6a38df082c25891e7c3f7
Aditya Raikar <raikaraditya8@gmail.com>
2026-09-23 23:41:51 +0530

    Add MoE model choices to training entry point

--- train.py
+++ train.py
@@ -27,8 +27,7 @@ class DeepLearningModel(pl.LightningModule):
         return self.model(x)
 
     def loss_function(self, cln_audio, enh_audio):
-        loss = self.si_sdr(cln_audio, enh_audio) + 25 * self.freqloss(cln_audio, enh_audio)
-        return loss
+        return self.si_sdr(cln_audio, enh_audio) + 25 * self.freqloss(cln_audio, enh_audio)
 
     def training_step(self, batch, batch_nb):
         enh_audio = self(batch['noisy'])
@@ -43,9 +42,9 @@ class DeepLearningModel(pl.LightningModule):
         return {'val_loss': loss}
 
     def on_validation_epoch_end(self, outputs):
-        avg_loss = torch.stack([x['val_loss'] for x in outputs]).mean()
-        tensorboard_logs = {'val_loss': avg_loss}
-        return {'val_loss': avg_loss, 'log': tensorboard_logs}
+        if outputs:
+            avg_loss = torch.stack([x['val_loss'] for x in outputs]).mean()
+            self.log('val_loss_epoch', avg_loss)
 
     def configure_optimizers(self):
         optimizer = torch.optim.Adam(self.parameters(), lr=3e-4, weight_decay=1e-5, betas=(0.5, 0.999))
@@ -55,53 +54,49 @@ class DeepLearningModel(pl.LightningModule):
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Speech Enhancement usnig SkipConv Net')
-    parser.add_argument('--model', type=str, help='ModelName', default='DCCTN_Modified')
+    parser = argparse.ArgumentParser(description='Speech Enhancement using complex DCCTN/MoE models')
+    parser.add_argument('--model', type=str, help='ModelName', default='DCCTN')
     parser.add_argument('--mode', type=str, help='Choose between "summary", "fast_run" or "train"', default='summary')
     parser.add_argument('--b', type=int, help='Batch Size', default=8)
-    parser.add_argument('--e', type=int, help='Epocs', default=2)
+    parser.add_argument('--e', type=int, help='Epochs', default=2)
     parser.add_argument('--gpu', type=str, help='GPU-IDs used to Train', default='0')
     parser.add_argument('--loss', type=str, help='loss function', default='SISDR+FreqLoss')
     args = parser.parse_args()
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-    if args.model == 'CFTNet':
-        from Network import CFTNet
-        curr_model = CFTNet()
-    elif args.model == 'DCCTN':
-        from Network import DCCTN
-        curr_model = DCCTN()
-    elif args.model == 'DATCFTNET':
-        from Network import DATCFTNET
-        curr_model = DATCFTNET()
-    elif args.model == 'DATCFTNET_DSC':
-        from Network import DATCFTNET_DSC
-        curr_model = DATCFTNET_DSC()
+    from Network import CFTNet, DCCTN, DATCFTNET, DATCFTNET_DSC
+    from moe_networks import DCCTN_SoftMoE, DCCTN_HardMoE
+    model_classes = {
+        'CFTNet': CFTNet,
+        'DCCTN': DCCTN,
+        'DATCFTNET': DATCFTNET,
+        'DATCFTNET_DSC': DATCFTNET_DSC,
+        'DCCTN_SoftMoE': DCCTN_SoftMoE,
+        'DCCTN_HardMoE': DCCTN_HardMoE,
+    }
+    if args.model not in model_classes:
+        raise ValueError('Unknown model {}. Choose from: {}'.format(args.model, ', '.join(model_classes)))
+    curr_model = model_classes[args.model]()
 
-    print("This process has the PID", os.getpid())
+    print('This process has the PID', os.getpid())
     model = DeepLearningModel(curr_model, args.b)
     print('Training Model: ' + model.modelname)
-    gpuIDs = [int(k) for k in args.gpu.split(' ')]
+    gpuIDs = [int(k) for k in args.gpu.split()]
     print('Training on GPU(s) : ', gpuIDs)
-    print('save model to : ' + os.getcwd() + '/Saved_Models/' + model.modelname)
-    callbacks = ModelCheckpoint(monitor='val_loss', dirpath=os.getcwd() + '/Saved_Models/' + model.modelname,
+    save_dir = os.path.join(os.getcwd(), 'Saved_Models', model.modelname)
+    os.makedirs(save_dir, exist_ok=True)
+    callbacks = ModelCheckpoint(monitor='val_loss', dirpath=save_dir,
                                 filename=model.modelname + '-DADX-IEEE-' + args.loss + '-{epoch:02d}-{val_loss:.2f}',
                                 save_top_k=1, mode='min')
-    TrainData = loaddataset(os.getcwd() + '/Database/Training_Samples/Train')
+    TrainData = loaddataset(os.path.join(os.getcwd(), 'Database/Training_Samples/Train'))
     trainloader = DataLoader(TrainData, batch_size=args.b, shuffle=True, num_workers=12, pin_memory=True)
-    DevData = loaddataset(os.getcwd() + '/Database/Training_Samples/Dev')
+    DevData = loaddataset(os.path.join(os.getcwd(), 'Database/Training_Samples/Dev'))
     devloader = DataLoader(DevData, batch_size=args.b, shuffle=False, num_workers=12, pin_memory=True)
-    print(gpuIDs)
     print(torch.cuda.is_available())
-    trainer = pl.Trainer(max_epochs=args.e, gpus=gpuIDs, strategy='ddp', callbacks=callbacks, gradient_clip_val=10,
-                         accumulate_grad_batches=8)  # GPU only
-    # trainer = pl.Trainer(max_epochs=args.e, accelerator="cpu", callbacks=callbacks, gradient_clip_val=10, accumulate_grad_batches=8) # CPU only
+    trainer = pl.Trainer(max_epochs=args.e, gpus=gpuIDs, strategy='ddp', callbacks=callbacks,
+                         gradient_clip_val=10, accumulate_grad_batches=8)
     trainer.fit(model, train_dataloaders=trainloader, val_dataloaders=devloader)
-    # shutil.rmtree(os.getcwd()+'/lightning_logs')
     print('Done!')
 
-# ------------------  How to Train -----------------------------------------------------
-# Run the following code in the terminal
-# python3 train.py --model "$model name" --b "$batch size" --e "$Num of epoch" --loss "$Loss Function" --gpu "$GPUs"
-# Example: python3 train.py --model CFTNet --b 8 --e 50 --loss SISDR+FreqLoss --gpu '0 1'
-# Simple Example: python3 train.py --model CFTNet
+# Examples:
+# python3 train.py --model DCCTN_SoftMoE --b 8 --e 50 --loss SISDR+FreqLoss --gpu '0 1'
+# python3 train.py --model DCCTN_HardMoE --b 8 --e 50 --loss SISDR+FreqLoss --gpu '0 1'
